5081533be52991429bfdb5d7e01e4c9e1e3fabd0
Pablo Pandolfo <ppandomail@gmail.com>
2025-08-29 12:47:19 -0300

    ejemplos de scanner en jflex y ply

---
+++ proy/proy-ply-calc/.ipynb_checkpoints/traductor-checkpoint.py
@@ -0,0 +1,93 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+tokens  = ('REVALUAR', 'PARIZQ', 'PARDER', 'CORIZQ', 'CORDER', 'MAS', 'MENOS', 'POR', 'DIVIDIDO', 'DECIMAL', 'ENTERO', 'PTCOMA')
+
+t_REVALUAR  = r'Evaluar'
+t_PARIZQ    = r'\('
+t_PARDER    = r'\)'
+t_CORIZQ    = r'\['
+t_CORDER    = r'\]'
+t_MAS       = r'\+'
+t_MENOS     = r'-'
+t_POR       = r'\*'
+t_DIVIDIDO  = r'/'
+t_PTCOMA    = r';'
+
+def t_DECIMAL(t):
+    r'\d+\.\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Floaat value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_ENTERO(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+    
+lexer = lex.lex()
+
+precedence = (
+    ('left','MAS','MENOS'),
+    ('left','POR','DIVIDIDO'),
+    ('right','UMENOS'),
+    )
+
+# Definición de la gramática
+def p_instrucciones_lista(t):
+    '''instrucciones    : instruccion instrucciones
+                        | instruccion '''
+
+def p_instrucciones_evaluar(t):
+    'instruccion : REVALUAR CORIZQ expresion CORDER PTCOMA'
+    print('El valor de la expresión es: ' + str(t[3]))
+
+def p_expresion_binaria(t):
+    '''expresion : expresion MAS expresion
+                | expresion MENOS expresion
+                | expresion POR expresion
+                | expresion DIVIDIDO expresion'''
+    if t[2] == '+'  : t[0] = t[1] + t[3]
+    elif t[2] == '-': t[0] = t[1] - t[3]
+    elif t[2] == '*': t[0] = t[1] * t[3]
+    elif t[2] == '/': t[0] = t[1] / t[3]
+
+def p_expresion_unaria(t):
+    'expresion : MENOS expresion %prec UMENOS'
+    t[0] = -t[2]
+
+def p_expresion_agrupacion(t):
+    'expresion : PARIZQ expresion PARDER'
+    t[0] = t[2]
+
+def p_expresion_number(t):
+    '''expresion    : ENTERO
+                    | DECIMAL'''
+    t[0] = t[1]
+
+def p_error(t):
+    print("Error sintáctico en '%s'" % t.value)
+
+parser = yacc.yacc()
+
+f = open("./entrada.txt", "r")
+input = f.read()
+print(input)
+parser.parse(input)
